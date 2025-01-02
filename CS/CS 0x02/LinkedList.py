feb0827827abad1243de6639d5efc839a63e487a
mk-isos <mkisos7900@naver.com>
2025-01-02 10:19:32 +0900

    250102 Create CS 0x02

---
+++ CS/CS 0x02/LinkedList.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+ll = LinkedList()
+ll.insert(1)
+ll.insert(2)
+print(ll.head.data)  # Output: 2
